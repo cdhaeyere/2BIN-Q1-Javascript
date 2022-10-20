204286fc00919d74fe17525f144e8b41fc881222
cdhaeyere <corentin.dhaeyere@student.vinci.be>
2022-10-20 12:29:54 +0200

    myMovies : module 5 : API with fat-model

---
+++ exercices/module-5/exercice-5.2/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const usersRouter = require('./routes/users');
+const filmRouter = require('./routes/films');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/users', usersRouter);
+app.use('/films', filmRouter);
+
+module.exports = app;
