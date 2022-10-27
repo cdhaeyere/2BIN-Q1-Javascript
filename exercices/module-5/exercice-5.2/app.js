8cf6f6e7593d0f0be074b7cb725bd380148cd330
cdhaeyere <corentin.dhaeyere@student.vinci.be>
2022-10-27 18:57:05 +0200

    myMovies : module 6 : full integration with RESTful API

--- exercices/module-5/exercice-5.2/app.js
+++ exercices/module-5/exercice-5.2/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
+
+const corsOptions = {
+  origin: 'http://localhost:8080',
+};
 
 const usersRouter = require('./routes/users');
 const filmRouter = require('./routes/films');
@@ -13,6 +18,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/users', usersRouter);
-app.use('/films', filmRouter);
+app.use('/films', cors(corsOptions), filmRouter);
 
 module.exports = app;
